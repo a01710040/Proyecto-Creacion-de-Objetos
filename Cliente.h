5cad5cc708affb75ec759b53f0b9b84694331670
a01710040 <a01710040@tec.mx>
2025-11-14 10:05:20 -0600

    Cliente.h
    Código para la clase Cliente

---
+++ Cliente.h
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+
+class Cliente {
+private:
+    string nombre;
+
+public:
+    // Declaraciones
+    Cliente(string p_nombre);
+    string getNombre();
+    void setNombre(string nuevoNombre);
+    void saludar();
+};
+
+// --- Desarrollo de la Función (Definición) ---
+
+Cliente::Cliente(string p_nombre) {
+    nombre = p_nombre;
+}
+
+string Cliente::getNombre() {
+    return nombre;
+}
+
+void Cliente::setNombre(string nuevoNombre) {
+    nombre = nuevoNombre;
+}
+
+void Cliente::saludar() {
+    cout << "Hola, me llamo " << nombre << "." << endl;
+}
