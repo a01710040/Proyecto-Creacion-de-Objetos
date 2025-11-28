128a89c8528a05dc25f4415bf4f6798f1b65e9ce
a01710040 <a01710040@tec.mx>
2025-11-28 09:32:58 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
-#include "Planta.h"
-#include "Maceta.h"
-#include "Cliente.h"
+#include "Vivero.h"
+
 using namespace std;
 
 int main() {
-    cout << "--- Probando Vivero (Versión Final Simplificada) ---" << endl << endl;
+    Vivero miNegocio;
 
-    // 1. Probar Planta
-    Planta miPlanta("Rosa", 50.0);
-    miPlanta.mostrar();
-    miPlanta.setPrecio(60.0);
-    cout << "El nuevo precio de la planta es: $" << miPlanta.getPrecio() << endl;
-    cout << "--------------------" << endl;
+    Planta p1("Orquidea", 150.0, 5);
+    Planta p2("Helecho", 80.0, 2);
+    
+    Maceta m1("Barro", "Arcilla", "Rojo", 40.0, false);
+    
+    Cliente c1("Ana", 200.0);
 
-    // 2. Probar Maceta
-    Maceta miMaceta("Barro", "Rojo");
-    miMaceta.mostrar();
-    miMaceta.setColor("Azul");
-    cout << "El nuevo color de la maceta es: " << miMaceta.getColor() << endl;
-    cout << "--------------------" << endl;
-    
-    // 3. Probar Cliente
-    Cliente miCliente("Luis");
-    miCliente.saludar();
-    miCliente.setNombre("Luis Pérez");
-    cout << "El nuevo nombre del cliente es: " << miCliente.getNombre() << endl;
-    cout << "--------------------" << endl;
-    
-    cout << "--- Pruebas finalizadas ---" << endl;
+    miNegocio.agregarPlanta(p1);
+    miNegocio.agregarPlanta(p2);
+    miNegocio.agregarMaceta(m1);
+    miNegocio.registrarCliente(c1);
+
+    miNegocio.mostrarInventario();
+
+    miNegocio.realizarVenta(0, 0);
+
+    cout << "\n--- DESPUES DE LA VENTA ---" << endl;
+    miNegocio.mostrarInventario();
 
     return 0;
 }
