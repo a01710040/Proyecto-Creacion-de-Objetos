4ee8445c69bb9275fccbf6cce29350dc51c15fd6
a01710040 <a01710040@tec.mx>
2025-12-01 18:12:47 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,49 +27,40 @@ int main() {
         cout << "Seleccione una opcion: ";
         cin >> opcion;
 
-        switch(opcion) {
-            case 1:
-                cout << "Nombre: "; cin >> nom;
-                cout << "Precio: "; cin >> pre;
-                cout << "Stock: "; cin >> stk;
-                miVivero.crearPlanta(nom, pre, stk);
-                break;
-
-            case 2:
-                cout << "Nombre: "; cin >> nom;
-                cout << "Material: "; cin >> mat;
-                cout << "Color: "; cin >> col;
-                cout << "Precio: "; cin >> pre;
-                cout << "Es colgante? (1=Si, 0=No): "; cin >> colg;
-                miVivero.crearMaceta(nom, mat, col, pre, colg);
-                break;
-
-            case 3: 
-                cout << "Nombre: "; cin >> nom;
-                cout << "Presupuesto: "; cin >> pre;
-                {
-                    Cliente nuevoC(nom, pre);
-                    miVivero.registrarCliente(nuevoC);
-                }
-                break;
-
-            case 4:
-                miVivero.mostrarInventario();
-                break;
-
-            case 5:
-                miVivero.mostrarInventario();
-                cout << "Indice del Cliente: "; cin >> idxCli;
-                cout << "Indice de la Planta: "; cin >> idxPlan;
-                miVivero.realizarVenta(idxCli, idxPlan);
-                break;
-
-            case 0:
-                cout << "Saliendo del sistema..." << endl;
-                break;
-
-            default:
-                cout << "Opcion no valida." << endl;
+        if (opcion == 1) {
+            cout << "Nombre: "; cin >> nom;
+            cout << "Precio: "; cin >> pre;
+            cout << "Stock: "; cin >> stk;
+            miVivero.crearPlanta(nom, pre, stk);
+        }
+        else if (opcion == 2) {
+            cout << "Nombre: "; cin >> nom;
+            cout << "Material: "; cin >> mat;
+            cout << "Color: "; cin >> col;
+            cout << "Precio: "; cin >> pre;
+            cout << "Es colgante? (1=Si, 0=No): "; cin >> colg;
+            miVivero.crearMaceta(nom, mat, col, pre, colg);
+        }
+        else if (opcion == 3) {
+            cout << "Nombre: "; cin >> nom;
+            cout << "Presupuesto: "; cin >> pre;
+            Cliente nuevoC(nom, pre);
+            miVivero.registrarCliente(nuevoC);
+        }
+        else if (opcion == 4) {
+            miVivero.mostrarInventario();
+        }
+        else if (opcion == 5) {
+            miVivero.mostrarInventario();
+            cout << "Indice del Cliente: "; cin >> idxCli;
+            cout << "Indice de la Planta: "; cin >> idxPlan;
+            miVivero.realizarVenta(idxCli, idxPlan);
+        }
+        else if (opcion == 0) {
+            cout << "Saliendo del sistema..." << endl;
+        }
+        else {
+            cout << "Opcion no valida. Intente de nuevo." << endl;
         }
 
     } while (opcion != 0);
