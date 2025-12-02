7ab0b01da992d7e3a1cb0789ea9d9e940b02a5ee
a01710040 <a01710040@tec.mx>
2025-12-01 19:43:35 -0600

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -17,31 +17,26 @@ public:
         presupuesto = 0.0;
     }
 
-    Cliente(string p_nombre, double p_presupuesto);
-    string getNombre();
-    bool comprar(double costo);
-    void mostrar();
-};
-
-Cliente::Cliente(string p_nombre, double p_presupuesto) {
-    nombre = p_nombre;
-    presupuesto = p_presupuesto;
-}
-
-string Cliente::getNombre() {
-    return nombre;
-}
-
-bool Cliente::comprar(double costo) {
-    if (presupuesto >= costo) {
-        presupuesto = presupuesto - costo;
-        return true;
+    Cliente(string p_nombre, double p_presupuesto) {
+        nombre = p_nombre;
+        presupuesto = p_presupuesto;
     }
-    return false;
-}
+    
+    string getNombre() {
+        return nombre;
+    }
 
-void Cliente::mostrar() {
-    cout << "Cliente: " << nombre << " | Saldo: $" << presupuesto << endl;
-}
+    bool comprar(double costo) {
+        if (presupuesto >= costo) {
+            presupuesto = presupuesto - costo;
+            return true;
+        }
+        return false;
+    }
+
+    void mostrar() {
+        cout << "Cliente: " << nombre << " | Saldo: $" << presupuesto << endl;
+    }
+};
 
 #endif
